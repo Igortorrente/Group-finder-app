0f3e86b185377afbd555a4e9f5c375ec090f9a68
igortorrente <igortorrente@hotmail.com>
2019-06-09 12:03:54 -0300

    Implement initial implementation of profile Fragment-Activity communication

--- GroupFinder/app/src/main/java/com/example/groupfinder/ProfileFragment.kt
+++ GroupFinder/app/src/main/java/com/example/groupfinder/ProfileFragment.kt
@@ -1,126 +1,103 @@
 package com.example.groupfinder
 
-import android.content.Context
+import android.app.Activity
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.Toast
 import androidx.annotation.Nullable
 import androidx.fragment.app.Fragment
-import com.example.groupfinder.base_classes.ApiHandler
-import com.example.groupfinder.base_classes.ApiUser
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.example.groupfinder.base_classes.Classes
+import com.example.groupfinder.base_classes.FinderViewModel
+import com.example.groupfinder.base_classes.UserData
 import kotlinx.android.synthetic.main.fragment_profile.*
 
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Activities that contain this fragment must implement the
- * [profileFragment.OnFragmentInteractionListener] interface
- * to handle interaction events.
- * Use the [profileFragment.newInstance] factory method to
- * create an instance of this fragment.
- *
- */
-class profileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-    private var listener: OnFragmentInteractionListener? = null
+class ProfileFragment : Fragment() {
+    private val profileEditFragmentRequestCode = 1
+    private lateinit var viewModel: FinderViewModel
+    private lateinit var userData: LiveData<UserData>
+    private lateinit var userClasses: LiveData<List<Classes>>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        viewModel = activity?.run {
+            ViewModelProviders.of(this).get(FinderViewModel::class.java)
+        }!!
 
+        userData = viewModel.userInfo
+        userClasses = viewModel.userClasses
+
+        userData.observe(this, Observer {
+            userData.value?.let {
+                subjectFieldTextView.text = userData.value!!.name
+                locationFieldTextView.text = userData.value!!.course
+                dataInitFieldTextView.text = userData.value!!.ra.toString()
+                Log.d("intent-user", userData.value.toString())
+            }
+        })
+
+        userClasses.observe(this, Observer {
+            //TODO: implement
+        })
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
 
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    fun onButtonPressed(uri: Uri) {
-        listener?.onFragmentInteraction(uri)
-    }
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (context is OnFragmentInteractionListener) {
-            listener = context
-        } else {
-            //throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
-        }
-    }
 
     override fun onActivityCreated(@Nullable savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        editProfileButton.setOnClickListener { v ->
-            val intent = Intent(v.context, profileEditActivity::class.java)
-            v.context.startActivity(intent)
-
-            val handler = ApiHandler()
-            handler.setContext(v.context)
-            handler.userAuth(ApiUser(ra = 177953, senha = "12345"))
+        userData.value?.let {
+            subjectFieldTextView.text = userData.value!!.name
+            locationFieldTextView.text = userData.value!!.course
+            dataInitFieldTextView.text = userData.value!!.ra.toString()
         }
 
-    }
-
-    override fun onDetach() {
-        super.onDetach()
-        listener = null
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     *
-     *
-     * See the Android Training lesson [Communicating with Other Fragments]
-     * (http://developer.android.com/training/basics/fragments/communicating.html)
-     * for more information.
-     */
-    interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        fun onFragmentInteraction(uri: Uri)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment profileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            profileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+        editProfileButton.setOnClickListener { view ->
+            val intent = Intent(view.context, ProfileEditActivity::class.java)
+            userData.value?.let {
+                intent.putExtra("userinfo", userData.value)
             }
+            userClasses.value?.let {
+                intent.putParcelableArrayListExtra("userclasses", userClasses.value as ArrayList<Classes>)
+            }
+            startActivityForResult(intent, profileEditFragmentRequestCode)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == profileEditFragmentRequestCode){
+            if(resultCode == Activity.RESULT_OK){
+                data?.let { data ->
+                    var userInfo = data.extras?.getParcelable("replyuserinfo") as UserData
+                    Log.d("intent-user", userInfo.toString())
+                    subjectFieldTextView.text = userInfo.name
+                    locationFieldTextView.text = userInfo.course
+                    dataInitFieldTextView.text = userInfo.ra.toString()
+
+                    subjectFieldTextView.refreshDrawableState()
+                    locationFieldTextView.refreshDrawableState()
+                    dataInitFieldTextView.refreshDrawableState()
+                    // TODO: add viewModel and user's Classes
+                }
+                Toast.makeText(this.context, "Sucesso !", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this.context, "Nenhuma mudança", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
--- GroupFinder/app/src/main/java/com/example/groupfinder/profileEditActivity.kt
+++ GroupFinder/app/src/main/java/com/example/groupfinder/profileEditActivity.kt
@@ -1,21 +1,54 @@
 package com.example.groupfinder
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Parcelable
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
+import com.example.groupfinder.base_classes.Classes
+import com.example.groupfinder.base_classes.UserData
+import kotlinx.android.synthetic.main.activity_profile_edit.*
+import java.util.ArrayList
 
-class profileEditActivity : AppCompatActivity() {
+class ProfileEditActivity : AppCompatActivity(){
+    private var userInfoHasChange: Boolean = true
+    private var userClassesHasChange: Boolean = false
+    private var classes: List<Classes>? = null
+    private var userInfo: UserData? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_edit)
 
-        // add back arrow to toolbar
-        if (supportActionBar != null){
-            supportActionBar!!.setDisplayHomeAsUpEnabled(true)
-            supportActionBar!!.setDisplayShowHomeEnabled(true)
+        val bundle: Bundle? = intent.extras
+        intent.extras?.let {
+            userInfo = intent.extras?.getParcelable("userinfo") as UserData
+            subjectFieldTextView.setText(userInfo?.name)
+            locationFieldTextView.setText(userInfo?.course)
+            dataInitFieldTextView.setText(userInfo?.ra.toString())
+
+            classes = intent.extras?.getParcelableArrayList<Classes>("userclasses") as List<Classes>
+
+            // TODO: Implement This classes
         }
+
+        subjectFieldTextView.addTextChangedListener {
+            userInfoHasChange = true
+        }
+        locationFieldTextView.addTextChangedListener {
+            userInfoHasChange = true
+        }
+        dataInitFieldTextView.addTextChangedListener {
+            userInfoHasChange = true
+        }
+
+        // add back arrow to toolbar
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -24,12 +57,22 @@ class profileEditActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        val id = item?.itemId
-        if(R.id.saveButton == id){
-            //TODO:
-        }
+        val replyIntent = Intent()
 
-        if (item!!.itemId == android.R.id.home) {
+        if(R.id.saveButton == item?.itemId){
+            if(!userInfoHasChange){
+                setResult(Activity.RESULT_CANCELED, replyIntent)
+            }else{
+                replyIntent.putExtra("replyuserinfo", UserData(dataInitFieldTextView.text.toString().toInt(),
+                    subjectFieldTextView.text.toString(), locationFieldTextView.text.toString(), userInfo!!.password))
+                // TODO: rework at this
+                replyIntent.putParcelableArrayListExtra("replyuserclasses", classes as ArrayList<out Parcelable>)
+                setResult(Activity.RESULT_OK, replyIntent)
+            }
+            finish()
+        }
+        if (item?.itemId == android.R.id.home) {
+            setResult(Activity.RESULT_CANCELED, replyIntent)
             finish() // close this activity and return to preview activity (if there is any)
         }
         return super.onOptionsItemSelected(item)
